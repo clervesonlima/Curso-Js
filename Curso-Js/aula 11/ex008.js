2ff97bc82150f46d2548c421b9b184000906d96c
Clerveson Ferreira De Lima Morais <mysmaguiwlima@gmail.com>
2023-01-07 11:36:37 -0300

    aula 11 concluída com sucesso

---
+++ Curso-Js/aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+if (vel > 60) {
+    console.log ("Você está acima da velocidade permitida e foi multado!")
+}
+console.log (`A velocidade do seu carro é de ${vel}Km/h`)
+console.log ('Dirija sempre usando cinto de segurança!')
